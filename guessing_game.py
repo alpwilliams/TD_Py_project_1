63de697913e3c40ed2338551480749572a63c9e1
alpwilliams <22511252+alpwilliams@users.noreply.github.com>
2020-03-10 11:16:29 +0900

    update 3
    added error tracking with some fun messages for spamming errors

--- guessing_game.py
+++ guessing_game.py
@@ -8,6 +8,7 @@ def start_game():
     high_score = 0
     guess_count = 1
     success = 0
+    error = 0
     play = 'y'
 
     # 2) Store a random number as the answer/solution
@@ -16,26 +17,44 @@ def start_game():
     # 3) Continuously prompt the player for a guess.
     while play.lower() == 'y':
         guess = input("Please guess a number between 1 and 10: ")
+        # Error counter (Easter Egg)
+        if error > 5 and error < 7:
+            print("Y tho?")
+            error = 7
+        elif error > 8 and error < 10:
+            print("Seriously...")
+            error = 9
+        elif error > 10:
+            print("Aight I'm out, system shutting down ☉_☉")
+            break
         try:
             guess = int(guess)
         except ValueError as err:
             print("Oh no, we ran into an issue. {}. Please try again".format(err))
         else:
             if guess < 1:
-                print("That guess is too low!")
+                print("That guess below 1!")
+                error = error + 1
             elif guess > 10:
-                print("That guess is too high!")
+                print("That guess is above 10!")
+                error = error + 1
+
             # 3a) If the guess greater than the solution, display to the player "It's lower"
             elif guess > answer:
                 print("It's lower.")
                 # increment number of guesses by 1
                 guess_count = guess_count + 1
+                # reset error counter
+                error = 0
                 continue
+
             # 3b) If the guess is less than the solution, display to the player "It's higher"
             elif guess < answer:
                 print("It's higher")
                 # increment number of guesses by 1
                 guess_count = guess_count + 1
+                # reset error counter
+                error = 0
                 continue
 
             # 4) Once the guess is correct, stop looping, inform the user they "Got it"
@@ -48,6 +67,7 @@ def start_game():
                     high_score = guess_count
                 elif guess_count < high_score:
                     high_score = guess_count
+
                 # 4a) Show how many attempts it took them to get the correct number
                 print("You took {} attempts to get the correct number.".format(guess_count))
                 # Display High Score
